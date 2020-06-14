db548c60fb1c132fda713a4406b52a0261bea1b1
Bibek Upreti <bibekupreti9813@gmail.com>
2020-06-14 22:03:21 +0545

    Variable

---
+++ Variable/variables.js
@@ -0,0 +1,11 @@
+
+
+var name = "Maria Jones";
+var age = 21.5;
+var hasDriverLicence = true;
+var empty = undefined;
+
+console.log(name + " type = " + typeof name);
+console.log(age + " type = " + typeof age);
+console.log(hasDriverLicence + " type = " + typeof hasDriverLicence);
+console.log(empty + " type = " + typeof empty);
