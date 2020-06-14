b386daec1e09631ccc52c396c6c08fb05cf5c9f1
Bibek Upreti <bibekupreti9813@gmail.com>
2020-06-14 22:32:15 +0545

    biultIn

---
+++ Built_in_Function/built-in-functions.js
@@ -0,0 +1,9 @@
+
+var person = {name: "Anna", age: 20};
+console.log(Object.keys(person));
+console.log(Object.values(person));
+
+console.log("James Bond".toLowerCase());
+console.log("James Bond".toUpperCase());
+console.log("James Bond".endsWith("Bond"));
+console.log("James Bond".startsWith("James"));
