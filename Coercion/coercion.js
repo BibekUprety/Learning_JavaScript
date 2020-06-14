8cb43ee7bde009e120bdd1d1d0115f306820ce92
Bibek Upreti <bibekupreti9813@gmail.com>
2020-06-14 22:48:52 +0545

    Coercion

---
+++ Coercion/coercion.js
@@ -0,0 +1,22 @@
+
+console.log(typeof 0 + " " + typeof false);
+console.log(0 == false);
+
+console.log(typeof "0" + " " + typeof false);
+console.log("0" == false);
+
+console.log(typeof 1 + " " + typeof "1");
+console.log(1 == "1");
+
+console.log("Without type coercion")
+
+console.log(typeof 0 + " " + typeof false);
+console.log(0 === false);
+
+console.log(typeof "0" + " " + typeof false);
+console.log("0" === false);
+
+console.log(typeof 1 + " " + typeof "1");
+console.log(1 === "1");
+
+
