a3945768aababdbb721d6696d9aa45c524ea5b27
Bibek Upreti <bibekupreti9813@gmail.com>
2020-06-14 22:00:49 +0545

    arithematicOperator

---
+++ Arithematic_Operator/arithematic-operators.js
@@ -0,0 +1,13 @@
+
+
+var addition = 2 + 2;
+var subtraction = 3 - 90;
+var division = 10 / 5;
+var multiplication = 3 * 30;
+var remainder = 10 % 4;
+
+console.log(addition);
+console.log(subtraction);
+console.log(division);
+console.log(multiplication);
+console.log(remainder);
