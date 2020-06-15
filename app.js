5f8323a6f0032341e775a5912888bf4370fca242
Bibek Upreti <57578727+BibekUprety@users.noreply.github.com>
2020-06-15 15:55:56 +0545

    ConatantKeyboard

---
+++ app.js
@@ -0,0 +1,15 @@
+// const name ="Bibek";
+
+// name ={};
+// name = 2;
+// console.log(name);
+
+const person ={};
+person['name']="Bibek";
+
+console.log(person.name);
+
+const array = [];
+array.push('Bibek from array');
+
+console.log(array[0]);
