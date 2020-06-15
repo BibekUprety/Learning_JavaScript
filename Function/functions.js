c5b9aa56ae2b0f4ae7ea8ee92d0cacd4f5252ccd
Bibek Upreti <57578727+BibekUprety@users.noreply.github.com>
2020-06-15 15:59:43 +0545

    function

---
+++ Function/functions.js
@@ -0,0 +1,12 @@
+
+function addNumbers(n1, n2) {
+  // start of function boddy
+  // Define logic
+  // 3 + 2
+  var result = n1+n2;
+  return result;
+  // end of function boddy
+}
+
+var result = addNumbers(10, 20);
+console.log(result)
